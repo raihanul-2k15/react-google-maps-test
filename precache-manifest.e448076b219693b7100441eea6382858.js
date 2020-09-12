98bd02af34c11979d4bca3bfb05bb57f2ca10a03
Raihanul Islam Refat <refat0167@gmail.com>
2020-09-12 10:17:59 +0600

    Updates

---
+++ precache-manifest.e448076b219693b7100441eea6382858.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b177729966a6ead4cde2731392b15bb8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2f1764d65d96fbe42b40",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cbbe6eb49f48540986ac",
+    "url": "./static/js/2.1d7edb6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1d7edb6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f1764d65d96fbe42b40",
+    "url": "./static/js/main.f90bf937.chunk.js"
+  },
+  {
+    "revision": "cb936f1dade835bb8ece",
+    "url": "./static/js/runtime-main.867f4f1c.js"
+  }
+]);
